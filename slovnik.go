37774635ed3be67f0af91a050748aba8c84400ac
Roman Peshkov <peshkovroman@gmail.com>
2017-06-13 22:49:25 +0200

    Added samples to word

--- slovnik.go
+++ slovnik.go
@@ -2,10 +2,18 @@ package slovnik
 
 // Word defines a structure with the word itself and possible translations of that word
 type Word struct {
-	Word         string   `json:"word"`
-	Translations []string `json:"translations"`
-	WordType     string   `json:"wordType"`
-	Synonyms     []string `json:"synonyms"`
-	Antonyms     []string `json:"antonyms"`
-	DerivedWords []string `json:"derivedWords"`
+	Word         string      `json:"word"`
+	Translations []string    `json:"translations"`
+	WordType     string      `json:"wordType"`
+	Synonyms     []string    `json:"synonyms"`
+	Antonyms     []string    `json:"antonyms"`
+	DerivedWords []string    `json:"derivedWords"`
+	Samples      []SampleUse `json:"samples"`
+}
+
+// SampleUse describes example phrase in which word can be used
+type SampleUse struct {
+	Keyword     string
+	Phrase      string
+	Translation string
 }
